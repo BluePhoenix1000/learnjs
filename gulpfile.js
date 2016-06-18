49645be9f7f17b65a08e65042c2784f88f2af063
BluePhoenix1000 <sirbralin.robertson@outlook.com>
2016-06-17 20:35:12 -0600

    version .01

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+const gulp = require('gulp');
+const babel = require('gulp-babel');
+
+// Gulp dependencies go here
+
+gulp.task('default', function() {
+   // Node Source
+   gulp.src('es6/**/*.js')
+    .pipe(babel())
+    .pipe(gulp.dest('dist'));
+
+  // Browser Source
+  gulp.src('public/es6/**/*.js')
+    .pipe(babel())
+    .pipe(gulp.dest('public/dist'));
+ });
